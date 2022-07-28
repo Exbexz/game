c7a8fa2ad2ef0b91b468e58b5776755ee4b54031
Exbexz <110139093+Exbexz@users.noreply.github.com>
2022-07-28 08:17:38 +0800

    Add files via upload

---
+++ Stats.gd
@@ -0,0 +1,15 @@
+extends Node
+
+export var max_health = 4
+onready var health = max_health setget set_health
+
+
+signal no_health
+signal health_changed(value)
+
+func set_health(value):
+	health = value
+	emit_signal("health_changed", health)
+	if health <= 0:
+		emit_signal("no_health")
+
